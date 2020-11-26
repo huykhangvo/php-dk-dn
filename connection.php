aa85e2cc9c8dc31e7105b73111b4a7baf00ede8e
Võ Huy Khang <47880923+huykhangvo@users.noreply.github.com>
2020-11-26 21:15:56 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $server_username = "sql9378643";  // Khai báo username
 $server_password = "t9axztCFH3"; // Khai báo password
 $server_host = "sql9.freemysqlhosting.net"; // Khai báo server
-$database = 'sql9378643';         // Khai báo database
+$database = 't9axztCFH3';         // Khai báo database
 // khởi tạo kết nối
 $conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
 mysqli_query($conn,"SET NAMES 'UTF8'");
