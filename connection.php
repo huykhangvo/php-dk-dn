1044e624e5c415bbb72722bbdd38e642df820f9a
Võ Huy Khang <47880923+huykhangvo@users.noreply.github.com>
2020-11-26 21:13:56 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$server_username = "epiz_27317056";  // Khai báo username
-$server_password = "T0qKyDwb0QXE3T"; // Khai báo password
-$server_host = "sql207.epizy.com"; // Khai báo server
-$database = 'epiz_27317056_huykhang';         // Khai báo database
+$server_username = "sql9378643";  // Khai báo username
+$server_password = "t9axztCFH3"; // Khai báo password
+$server_host = "sql9.freemysqlhosting.net"; // Khai báo server
+$database = 'sql9378643';         // Khai báo database
 // khởi tạo kết nối
 $conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
 mysqli_query($conn,"SET NAMES 'UTF8'");
